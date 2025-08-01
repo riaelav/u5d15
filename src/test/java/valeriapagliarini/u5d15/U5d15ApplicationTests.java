b02c4c93d3bfcfa7efadc15da79a23bac20e6474
Valeria Pagliarini <valepagliar@gmail.com>
2025-08-01 09:43:31 +0100

    Initial commit

---
+++ src/test/java/valeriapagliarini/u5d15/U5d15ApplicationTests.java
@@ -0,0 +1,13 @@
+package valeriapagliarini.u5d15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
